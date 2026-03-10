5cf6c4620f41ccfdf68b07804ca03c6981527f4f
huydt <lshuydt@gmail.com>
2026-03-10 11:29:23 +0700

    add setup-env

--- setup-env.bat
+++ setup-env.bat
@@ -1,25 +1,51 @@
-@echo off
-echo Installing JDK 21...
+#!/usr/bin/env bash
 
-set JDK_URL=https://github.com/adoptium/temurin21-binaries/releases/latest/download/OpenJDK21U-jdk_x64_windows_hotspot.zip
-set JDK_DIR=%USERPROFILE%\jdk21
+set -e
 
-echo Downloading JDK 21...
-powershell -Command "Invoke-WebRequest -Uri %JDK_URL% -OutFile jdk21.zip"
+echo "Installing JDK 21 and Gradle..."
 
-echo Extracting JDK...
-powershell -Command "Expand-Archive jdk21.zip -DestinationPath %USERPROFILE% -Force"
+# install dependencies
+sudo apt-get update
+sudo apt-get install -y wget unzip
 
-for /d %%i in (%USERPROFILE%\jdk-21*) do set JDK_DIR=%%i
+# -------------------------
+# Install JDK 21
+# -------------------------
+JDK_URL="https://github.com/adoptium/temurin21-binaries/releases/latest/download/OpenJDK21U-jdk_x64_linux_hotspot.tar.gz"
 
-echo Setting environment variables...
-setx JAVA_HOME "%JDK_DIR%"
-setx PATH "%JDK_DIR%\bin;%PATH%"
+wget -O jdk21.tar.gz $JDK_URL
+tar -xzf jdk21.tar.gz
 
-echo Cleaning up...
-del jdk21.zip
+JDK_DIR=$(ls -d jdk-21*)
 
-echo Verifying installation...
-"%JDK_DIR%\bin\java" -version
+sudo mv $JDK_DIR /opt/jdk21
 
-echo JDK 21 installation completed!
+echo "export JAVA_HOME=/opt/jdk21" >> ~/.bashrc
+echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> ~/.bashrc
+
+export JAVA_HOME=/opt/jdk21
+export PATH=$JAVA_HOME/bin:$PATH
+
+# -------------------------
+# Install Gradle
+# -------------------------
+GRADLE_VERSION=8.7
+wget https://services.gradle.org/distributions/gradle-${GRADLE_VERSION}-bin.zip
+
+unzip gradle-${GRADLE_VERSION}-bin.zip
+sudo mv gradle-${GRADLE_VERSION} /opt/gradle
+
+echo "export GRADLE_HOME=/opt/gradle" >> ~/.bashrc
+echo "export PATH=\$GRADLE_HOME/bin:\$PATH" >> ~/.bashrc
+
+export GRADLE_HOME=/opt/gradle
+export PATH=$GRADLE_HOME/bin:$PATH
+
+# cleanup
+rm jdk21.tar.gz
+rm gradle-${GRADLE_VERSION}-bin.zip
+
+echo "-------------------------"
+java -version
+gradle -v
+echo "Installation completed!"
